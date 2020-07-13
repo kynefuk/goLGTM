5130ef2126e9b80ecc1f91a39e9cca1603288bc1
kynefuk <atsuminamihira@gmail.com>
2020-07-13 22:27:22 +0900

    move putting message on img process to processor package

--- imgsource/local.go
+++ imgsource/local.go
@@ -3,18 +3,13 @@ package imgsource
 import (
 	"bufio"
 	"fmt"
-	"golang.org/x/image/math/fixed"
 	"image"
-	"image/draw"
 	"image/png"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/golang/freetype"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/gofont/gobold"
+	"github.com/kynefuk/goLGTM/processor"
 )
 
 // MaxRatio is a ratio of messaging aria
@@ -33,7 +28,6 @@ type LocalImage struct {
 
 // AddMessage adds message to image
 func (localImg *LocalImage) AddMessage(message string) error {
-	var text = []string{message}
 
 	file, err := os.Open(localImg.source)
 	defer file.Close()
@@ -46,59 +40,10 @@ func (localImg *LocalImage) AddMessage(message string) error {
 		return fmt.Errorf("failed to decode image. error: %s", err)
 	}
 
-	// message aria
-	msgAriaWidth := float64(img.Bounds().Dy()) * MaxRatio
-
-	// parse font
-	ft, err := truetype.Parse(gobold.TTF)
+	msgProcessor := processor.NewProcessor(img, message)
+	rgba, err := msgProcessor.AddMessage()
 	if err != nil {
-		return fmt.Errorf("failed to parse font file. error: %s", err)
-	}
-
-	rgba := image.NewRGBA(image.Rect(0, 0, img.Bounds().Dx(), img.Bounds().Dy()))
-
-	var fontSize float64
-	var messageXPoint int
-	for i := MaxFontSize; i > MinFontSize; i-- {
-		opt := truetype.Options{
-			Size:              i,
-			DPI:               72,
-			Hinting:           font.HintingVertical,
-			GlyphCacheEntries: 0,
-			SubPixelsX:        0,
-			SubPixelsY:        0,
-		}
-		face := truetype.NewFace(ft, &opt)
-		dr := &font.Drawer{Dst: rgba, Src: img, Face: face, Dot: fixed.Point26_6{}}
-		messageXPoint = ((fixed.I(img.Bounds().Dx()) - dr.MeasureString(message)) / 2).Floor()
-
-		messageAria := int(msgAriaWidth) - dr.MeasureString(message).Floor()
-		if messageAria > 0 {
-			fontSize = i
-			break
-		}
-	}
-
-	_, fw := image.Black, image.White
-	draw.Draw(rgba, rgba.Bounds(), img, image.ZP, draw.Src)
-	c := freetype.NewContext()
-	c.SetDPI(72)
-	c.SetFont(ft)
-	c.SetFontSize(float64(fontSize))
-	c.SetClip(rgba.Bounds())
-	c.SetDst(rgba)
-	c.SetSrc(fw)
-	c.SetHinting(font.HintingVertical)
-
-	messageYPoint := img.Bounds().Dy() / 2
-	// Draw the text.
-	pt := freetype.Pt(messageXPoint, messageYPoint)
-	for _, s := range text {
-		_, err = c.DrawString(s, pt)
-		if err != nil {
-			return fmt.Errorf("failed to create out file. error: %s", err)
-		}
-		pt.Y += c.PointToFixed(12 * 1.5)
+		return fmt.Errorf("failed to process message. error: %s", err)
 	}
 
 	// Save that RGBA image to disk.
@@ -107,6 +52,7 @@ func (localImg *LocalImage) AddMessage(message string) error {
 		return fmt.Errorf("failed to create out file. error: %s", err)
 	}
 	defer outFile.Close()
+
 	b := bufio.NewWriter(outFile)
 	err = png.Encode(b, rgba)
 	if err != nil {
@@ -116,14 +62,8 @@ func (localImg *LocalImage) AddMessage(message string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create out file. error: %s", err)
 	}
-	fmt.Println("Wrote out.png OK.")
 
 	return nil
-}
-
-// NewLocalImage is a constructor of LocalImage
-func NewLocalImage(source string) *LocalImage {
-	return &LocalImage{source: source}
 }
 
 func newImgName(source string) string {
@@ -132,6 +72,11 @@ func newImgName(source string) string {
 	fileExt := filepath.Ext(source)
 	fileName = strings.Replace(fileName, fileExt, "", 1)
 	return filePath + "/" + fileName + "_LGTM_" + fileExt
+}
+
+// NewLocalImage is a constructor of LocalImage
+func NewLocalImage(source string) *LocalImage {
+	return &LocalImage{source: source}
 }
 
 // ft, err := truetype.Parse(gobold.TTF)
---
+++ processor/process.go
@@ -0,0 +1,94 @@
+package processor
+
+import (
+	"fmt"
+	"golang.org/x/image/math/fixed"
+	"image"
+	"image/draw"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/gobold"
+)
+
+// MaxRatio is a ratio of messaging aria
+const MaxRatio float64 = 0.8
+
+// MaxFontSize is max size of font size
+const MaxFontSize float64 = 100
+
+// MinFontSize is minimum size of font size
+const MinFontSize float64 = 1
+
+// Processor put message on image
+type Processor struct {
+	img     image.Image
+	message string
+}
+
+// AddMessage put message on image
+func (p *Processor) AddMessage() (*image.RGBA, error) {
+	var text = []string{p.message}
+	// message aria
+	msgAriaWidth := float64(p.img.Bounds().Dy()) * MaxRatio
+
+	// parse font
+	ft, err := truetype.Parse(gobold.TTF)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse font file. error: %s", err)
+	}
+
+	rgba := image.NewRGBA(image.Rect(0, 0, p.img.Bounds().Dx(), p.img.Bounds().Dy()))
+
+	var fontSize float64
+	var messageXPoint int
+	for i := MaxFontSize; i > MinFontSize; i-- {
+		opt := truetype.Options{
+			Size:              i,
+			DPI:               72,
+			Hinting:           font.HintingVertical,
+			GlyphCacheEntries: 0,
+			SubPixelsX:        0,
+			SubPixelsY:        0,
+		}
+		face := truetype.NewFace(ft, &opt)
+		dr := &font.Drawer{Dst: rgba, Src: p.img, Face: face, Dot: fixed.Point26_6{}}
+		messageXPoint = ((fixed.I(p.img.Bounds().Dx()) - dr.MeasureString(p.message)) / 2).Floor()
+
+		messageAria := int(msgAriaWidth) - dr.MeasureString(p.message).Floor()
+		if messageAria > 0 {
+			fontSize = i
+			break
+		}
+	}
+
+	_, fw := image.Black, image.White
+	draw.Draw(rgba, rgba.Bounds(), p.img, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(72)
+	c.SetFont(ft)
+	c.SetFontSize(float64(fontSize))
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fw)
+	c.SetHinting(font.HintingVertical)
+
+	messageYPoint := p.img.Bounds().Dy() / 2
+	// Draw the text.
+	pt := freetype.Pt(messageXPoint, messageYPoint)
+	for _, s := range text {
+		_, err = c.DrawString(s, pt)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create out file. error: %s", err)
+		}
+		pt.Y += c.PointToFixed(12 * 1.5)
+	}
+
+	return rgba, nil
+}
+
+// NewProcessor is a constructor of Processor
+func NewProcessor(img image.Image, message string) *Processor {
+	return &Processor{img: img, message: message}
+}
